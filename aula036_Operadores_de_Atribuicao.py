44b1636e81fbca9e2d43945c64bbd4cb23d352b6
Maicon D R Oliva <maiconstr@outlook.com>
2023-06-22 20:45:07 -0300

    Atual

---
+++ aula036_Operadores_de_Atribuicao.py
@@ -0,0 +1,17 @@
+"""
+Operadores de atribuição
+= Sinal de Igual 
++= Usa o valor antigo para fazer uma soma
+-= Usa o valor antigo para fazer uma subtração
+*= Usa o valor antigo para fazer uma multiplicação
+/= Divisão
+//= Divisão inteira
+**= Potenciação 
+%= Módulo
+"""
+contador = 10
+
+###
+
+contador /= 5
+print(contador)
